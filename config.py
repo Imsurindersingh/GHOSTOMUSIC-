639ef2fc602cdb48b754e8abb259446fafc3b1a7
Imsurindersingh <imsurindersingh0.0@gmail.com>
2025-03-26 15:34:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,19 @@ load_dotenv()
 
 class Config(object):
     # required config variables
-    API_HASH = getenv("API_HASH", None)                # get from my.telegram.org
-    API_ID = int(getenv("API_ID", 0))                  # get from my.telegram.org
-    BOT_TOKEN = getenv("BOT_TOKEN", None)              # get from @BotFather
-    DATABASE_URL = getenv("DATABASE_URL", None)        # from https://cloud.mongodb.com/
-    HELLBOT_SESSION = getenv("HELLBOT_SESSION", None)  # enter your session string here
-    LOGGER_ID = int(getenv("LOGGER_ID", 0))            # make a channel and get its ID
-    OWNER_ID = getenv("OWNER_ID", "")                  # enter your id here
-
+    API_HASH = getenv("f182fe949bf71f0ed098e14d28c1ce28", None)                # get from my.telegram.org
+    API_ID = int(getenv("28260914", 0))                  # get from my.telegram.org
+    BOT_TOKEN = getenv("7770806448:AAFOduyOZC1hBgYzOrswGELR9VScCTc9_nY", None)              # get from @BotFather
+    DATABASE_URL = getenv("mongodb+srv://GHOSTOMUSIC:CiyvvxoYAS01Pezm@ghosto-omusic.rniqbaf.mongodb.net/GHOSTO_DB?retryWrites=true&w=majority&appName=GHOSTO-OMUSIC", None)        # from https://cloud.mongodb.com/
+    HELLBOT_SESSION = getenv("BQGvOjIAvwfqNSJ9cuPAE-kIMlgaPDj7Iax9lcsPfIZTnQn1ATIUv8hdt5AKO5GbG1WeGw24pncK5JLKphB67hXDmd_sDsO6O94Et07xFdh8esc3TbkevPNkjRrLv4YoA5N7IIWNPsXOHUtG79g7SyVDoXYWOhZ9O9gnPXo0VkfZYhLSxzTVCPumQu_XSfkA5qdZwZh9P8AGWAL58HYSa32_WmApMefhMUMFSemHMuiMhnpLWzA4MQ19xATKEY4dt1Teh-HYi3cdAy41hIy5La3RjYSMf2YFUdB9-KCOjAeiHlzlq7Sw4us03qkCxd2FEo_mdDYVufzvKyZBaHgVHzNnxWlLqQAAAAHPLRiwAQ", None)  # enter your session string here
+    LOGGER_ID = int(getenv("-1002566517734", 0))            # make a channel and get its ID
+    OWNER_ID = getenv("7703788539", "")  # enter your id here
+    GROUP ID = getenv("-1002592634422", 0))  #enter you group logger id
+    
     # optional config variables
     BLACK_IMG = getenv("BLACK_IMG", "https://telegra.ph/file/2c546060b20dfd7c1ff2d.jpg")        # black image for progress
     BOT_NAME = getenv("BOT_NAME", "\x40\x4d\x75\x73\x69\x63\x5f\x48\x65\x6c\x6c\x42\x6f\x74")   # dont put fancy texts here.
-    BOT_PIC = getenv("BOT_PIC", "https://te.legra.ph/file/5d5642103804ae180e40b.jpg")           # put direct link to image here
+    BOT_PIC = getenv("BOT_PIC", "https://graph.org/file/4d1467439269f27634331-1ac15481f1992f853d.jpg")           # put direct link to image here
     LEADERBOARD_TIME = getenv("LEADERBOARD_TIME", "3:00")   # time in 24hr format for leaderboard broadcast
     LYRICS_API = getenv("LYRICS_API", None)             # from https://docs.genius.com/
     MAX_FAVORITES = int(getenv("MAX_FAVORITES", 30))    # max number of favorite tracks
